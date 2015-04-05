a58a93d81933346ef59479178254e15e10b146cc
wilfredo393 <wilfredo393@hotmail.com>
2015-04-05 15:20:32 -0600

    Se modifico el redirect a la pagina de login

--- Capa Presentacion/Inicio.aspx.vb
+++ Capa Presentacion/Inicio.aspx.vb
@@ -1,22 +1,20 @@
-﻿Imports System.Web.HttpResponse
-
+﻿
 Public Class Inicio
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
 
-        'Dim userid As String
+        Dim userid As String
 
-        'userid = Session["UsuarioID"]
+        userid = Session("UsuarioID")
 
-        'If (!IsPostBack) Then
+        If (Not IsPostBack) Then
 
-        '    If (userid = "" Or userid = IsNull) Then
-        Response.Redirect("../Login.aspx")
+            If (userid = "" Or userid = Nothing) Then
+                Response.Redirect("~/Login.aspx")
+            End If
 
-        '    End If
-
-        'End If
+        End If
 
     End Sub
 End Class
